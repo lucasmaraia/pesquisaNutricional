663a3a4c35f704fe12f0eefe550f4e5ed3d04e47
lucasmaraia <l.cliniomaraia@gmail.com>
2021-03-01 22:27:05 -0300

    data cadastro

--- salva_mensagem.php
+++ salva_mensagem.php
@@ -5,9 +5,10 @@
     $pergunta3 = $_POST['pergunta3'];
     $pergunta4 = $_POST['pergunta4'];
     $pergunta5 = $_POST['pergunta5'];
+    
 
-    $result_msg_contato = "insert into formulario(pergunta1,pergunta2,pergunta3,pergunta4,pergunta5) 
-    values('$pergunta1','$pergunta2','$pergunta3','$pergunta4','$pergunta5')";
+    $result_msg_contato = "insert into formulario(pergunta1,pergunta2,pergunta3,pergunta4,pergunta5,datacadastro) 
+    values('$pergunta1','$pergunta2','$pergunta3','$pergunta4','$pergunta5','now()')";
 
     $resultado_msg_contato = mysqli_query($mysqli,$result_msg_contato)
 
